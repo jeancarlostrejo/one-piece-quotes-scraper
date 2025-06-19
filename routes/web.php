14e382f31e67b29f873c92607d25678601d60e30
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2025-06-19 03:26:15 -0400

    feat: update welcome page layout for random quote endpoints

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,4 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome')->name('home');
 
 Route::get('/scraping', [ScrapingController::class, 'scrapeQuotes'])->name('scraping.scrapeQuotes');
-Route::get('/quotes/random', [QuoteController::class, 'randomQuote'])->name('quotes.random');
+Route::get('/quote-random', [QuoteController::class, 'randomQuote'])->name('quote.random');
