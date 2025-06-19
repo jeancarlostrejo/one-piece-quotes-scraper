723e6736d4695387b2465578548bca49dd041065
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2025-06-19 09:03:22 -0400

    add cache implementation

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Storage;
 
 class QuoteController extends Controller
@@ -17,8 +18,11 @@ class QuoteController extends Controller
             ], 404);
         }
 
-        $jsonData = Storage::disk('quotes')->get('quotes.json');
-        $quotes = json_decode($jsonData, true);
+        $quotes = Cache::remember('quotes_data', now()->addDay(), function() {
+            $jsonData = Storage::disk('quotes')->get('quotes.json');
+
+            return json_decode($jsonData, true);
+        });
 
         return response()->json($quotes);
     }
@@ -31,8 +35,11 @@ class QuoteController extends Controller
             ], 404);
         }
 
-        $jsonData = Storage::disk('quotes')->get('quotes.json');
-        $quotes = collect(json_decode($jsonData, true));
+        $quotes = Cache::remember('quotes_data', now()->addDay(), function() {
+            $jsonData = Storage::disk('quotes')->get('quotes.json');
+
+            return collect(json_decode($jsonData, true));
+        });
 
         $quote = $quotes->random();
 
@@ -47,8 +54,11 @@ class QuoteController extends Controller
             ], 404);
         }
 
-        $jsonData = Storage::disk('quotes')->get('quotes.json');
-        $quotes = collect(json_decode($jsonData, true));
+        $quotes = Cache::remember('quotes_data', now()->addDay(), function() {
+            $jsonData = Storage::disk('quotes')->get('quotes.json');
+
+            return json_decode($jsonData, true);
+        });
 
         $quote = $quotes->random();
 
