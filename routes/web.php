8be39c477ef9bc59a6870a090865273e545aa487
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2025-06-19 03:17:22 -0400

    feat: add random quote functionality and fix error with <br> tag

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
+use App\Http\Controllers\QuoteController;
 use App\Http\Controllers\ScrapingController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome')->name('home');
 
 Route::get('/scraping', [ScrapingController::class, 'scrapeQuotes'])->name('scraping.scrapeQuotes');
+Route::get('/quotes/random', [QuoteController::class, 'randomQuote'])->name('quotes.random');
